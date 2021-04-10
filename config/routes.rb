c62fcfb2fcd4bf8565b3007a71af83c0550344e3
Gillys Dantas <gillys.ayres@gmail.com>
2021-04-10 12:27:03 -0300

    Created show task route

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # verb 'path', to: 'controller#action'
+  # Display all tasks
+  get "tasks", to: "tasks#index"
+  # Display one task
+  get "tasks", to: "tasks#index", as: :task
 end
