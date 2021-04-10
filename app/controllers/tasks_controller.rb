17e05f11e84a63c72df7080164a843bda67d3b8c
Gillys Dantas <gillys.ayres@gmail.com>
2021-04-10 14:08:09 -0300

    Fixed show route

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,4 +4,8 @@ class TasksController < ApplicationController
     @tasks = Task.all
   end
 
+  def show
+    @task = Task.find(params[:id])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # verb 'path', to: 'controller#action'
   # Display all tasks
-  get "tasks", to: "tasks#index"
+  get 'tasks', to: 'tasks#index'
   # Display one task
-  get "tasks", to: "tasks#index", as: :task
+  get 'tasks/:id', to: 'tasks#show', as: :task
 end
